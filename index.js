001c352c5e59146de05c543c480234b6049bdea8
Ilya Volodin <ivolodin@gmail.com>
2014-09-15 21:56:54 -0400

    Adding all rules to export object

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 module.exports = {
     rules: {
-        "model-defaults": require("./lib/rules/model-defaults")
+        "collection-model": require("./lib/rules/collection-model"),
+        "defaults-on-top": require("./lib/rules/defaults-on-top"),
+        "model-defaults": require("./lib/rules/model-defaults"),
+        "no-constructor": require("./lib/rules/no-constructor"),
+        "no-native-jquery": require("./lib/rules/no-native-jquery")
     }
 };
